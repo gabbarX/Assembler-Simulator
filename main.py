161370dfdee0af4ec58f91773b0a803aec89cd9d
Ankit Gautam <ankit21518@iiitd.ac.in>
2022-07-03 16:17:35 +0530

    variable handled

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ registors = {
     "R6": "110",
     "FLAGS": "111"}
 
+
 #Mention of Instructions
-codes={
-    "add": ["10000", "A"],
+codes =  {
+    "add": ["10000", "A"],        
     "sub": ["10001", "A"],
     "mov": ["10010", "B"],
     "mov": ["10011", "C"],
     "ld": ["10100", "D"],
-    "st": ["10001", "D"],
+    "st": ["10101", "D"],
     "mul": ["10110", "A"],
     "div": ["10111", "C"],
     "rs": ["11000", "B"],
@@ -32,7 +33,6 @@ codes={
     "je": ["01111", "E"],
     "hlt": ["01010", "F"],
 }
-
 
 #Functions
 def decimalTo8bitBinary(num):
@@ -54,7 +54,12 @@ def printTypeA(opcode, reg1, reg2, reg3):
 def printTypeB(opcode, reg1, value):
     n = int(value[1::])
     if (n <= 255 and n >= 0):
-        print(f"{codes[opcode][0]}{registors[reg1]}{decimalTo8bitBinary(value[1::])}")
+        if (opcode == "mov"):
+            print(f"10010{registors[reg1]}{decimalTo8bitBinary(value[1::])}")
+        elif (opcode == "rs"):
+            print(f"11000{registors[reg1]}{decimalTo8bitBinary(value[1::])}")
+        elif (opcode == "ls"):
+            print(f"11001{registors[reg1]}{decimalTo8bitBinary(value[1::])}")
     else:
         print("\nERROR\nIllegal Immediate Value used!")
         exit()
@@ -65,37 +70,56 @@ def printTypeC(opcode, reg1, reg2):
 
 
 def printTypeD(opcode, reg1, address):
-    print(f"{codes[opcode][0]}{registors[reg1]}{address}")
+    print(f"{codes[opcode][0]}{registors[reg1]}{decimalTo8bitBinary(var.get(address))}")
 
 
 def printTypeE(opcode, address):
-    print(f"{codes[opcode][0]}000{address}")
+    if (opcode == "jmp"):
+        print(f"11111000{decimalTo8bitBinary(label.get(address))}")
+    elif (opcode == "jlt"):
+        print(f"01100000{decimalTo8bitBinary(label.get(address))}")
+    elif (opcode == "jgt"):
+        print(f"01101000{decimalTo8bitBinary(label.get(address))}")
+    elif (opcode == "je"):
+        print(f"01111000{decimalTo8bitBinary(label.get(address))}")
 
 
 def printTypeF(opcode):
     print(f"{codes[opcode][0]}00000000000")
 
 
-variables=[]
-variablecount=0
+label = {}
+var = {}
+variables = []
+variablecount = 0
 track = 0
+
 
 # Main program
 if __name__== "__main__":
     print("Welcome to the Assembler!")
     with open("practiseInput.txt", "r") as file:
-        data = file.read().split("\n")
-    data.pop()
-
+        d = (file.read()).split("\n")
+    data = []
+    for i in d:
+        if (len(i) != 0):
+            data.append(i)
+    for i in data:
+        print(i)
+    for i in range(len(data)):
+        if (data[i].find(":") != -1):
+            index = i + 1
+            label.update({str(data[i][:data[i].find(":")]): str(index)})
     print("\nMachine Code:-")
     try:
         for i in data:
             temp = list(i.split())
-            # print(temp)
+            print(temp[0])
             if temp[0] == 'var':
                 variables.append(temp[1])
-                variablecount += 1
+                variablecount +=1
                 track += 1
+                var.update({str(variables[variablecount - 1]): str(len(data) - variablecount)})
                 continue
 
             if temp[0] == "mov":
@@ -128,9 +152,10 @@ if __name__== "__main__":
                     print("\nERROR\nUndefined Varaible Used!")
                     exit()
 
-            if codes[temp[0]][1] == 'E':
-                printTypeE(temp[0], temp[1])
+            if temp[0] == "jmp" or temp[0] == "jlt" or temp[0] == "jgt" or temp[0] == "je":
+                printTypeB(temp[0], temp[1])
                 track += 1
+                continue
 
             if codes[temp[0]][1] == 'F':
                 printTypeF(temp[0])
