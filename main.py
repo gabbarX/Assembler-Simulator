8cdb705fc92cab48b78ca866fb5b9c87afcd7e9a
Ankit Gautam <ankit21518@iiitd.ac.in>
2022-07-05 14:06:18 +0530

    updated

--- main.py
+++ main.py
@@ -88,93 +88,138 @@ def printTypeF(opcode):
     print(f"{codes[opcode][0]}00000000000")
 
 
-label = {}
-var = {}
-variables = []
-variablecount = 0
-track = 0
-lables={}
 
 # Main program
-if __name__== "__main__":
-    print("Welcome to the Assembler!")
-    with open("practiseInput.txt", "r") as file:
-        d = (file.read()).split("\n")
-    data = []
-    for i in d:
-        if (len(i) != 0):
-            data.append(i)
-    for i in data:
-        print(i)
-    for i in range(len(data)):
-        if (data[i].find(":") != -1):
-            index = i + 1
-            label.update({str(data[i][:data[i].find(":")]): str(index)})
-    print("\nMachine Code:-")
-    try:
-        for i in data:
-            temp = list(i.split())
-            print(temp[0])
-            if temp[0] == 'var':
-                variables.append(temp[1])
-                variablecount +=1
-                track += 1
-                var.update({str(variables[variablecount - 1]): str(len(data) - variablecount)})
+# if __name__== "__main__":
+#     print("Welcome to the Assembler!")
+#     with open("practiseInput.txt", "r") as file:
+#         d = (file.read()).split("\n")
+#     data = []
+#     for i in d:
+#         if (len(i) != 0):
+#             data.append(i)
+#     # for i in data:
+#     #     print(i)
+#     for i in range(len(data)):
+#         if (data[i].find(":") != -1):
+#             index = i + 1
+#             label.update({str(data[i][:data[i].find(":")]): str(index)})
+#     print("\nMachine Code:-")
+#     try:
+#         for i in data:
+#             temp = list(i.split())
+#             print(temp)
+#             if ':' in temp:
+#                 label.append(temp[1])
+#                 variablecount +=1
+#                 track += 1
+#                 var.update({str(variables[variablecount - 1]): str(len(data) - variablecount)})
+#                 continue
+
+#             if temp[0] == "mov":
+#                 if (temp[2][0]) == "$":
+#                     printTypeB(temp[0], temp[1], temp[2])
+#                     track += 1
+#                     continue
+#                 else:
+#                     printTypeC(temp[0], temp[1], temp[2])
+#                     track += 1
+#                     continue
+
+#             if codes[temp[0]][1] == 'A':
+#                 printTypeA(temp[0], temp[1], temp[2], temp[3])
+#                 track += 1
+
+#             if codes[temp[0]][1] == 'B':
+#                 printTypeB(temp[0], temp[1], temp[2])
+#                 track += 1
+
+#             if codes[temp[0]][1] == 'C':
+#                 printTypeC(temp[0], temp[1], temp[2])
+#                 track += 1
+
+#             if (codes[temp[0]][1] == 'D'):
+#                 if (temp[2] in variables):
+#                     printTypeD(temp[0], temp[1], temp[2])
+#                     track += 1
+#                 else:
+#                     print("\nERROR\nUndefined Varaible Used!")
+#                     exit()
+
+#             if temp[0] == "jmp" or temp[0] == "jlt" or temp[0] == "jgt" or temp[0] == "je":
+#                 printTypeB(temp[0], temp[1])
+#                 track += 1
+#                 continue
+
+#             if codes[temp[0]][1] == 'F':
+#                 printTypeF(temp[0])
+#                 track += 1
+#                 break
+
+#         if track != len(data):
+#             print("\nERROR\nLast Instruction is required to be HLT")
+#             exit()
+#         if temp != ['hlt']:
+#             print("\nERROR\nHLT Instruction Missing!")
+#         else:
+#             print(f"\nNumber of Variables: {variablecount}")
+#             print("Variables:-", end = " ")
+#             for i in variables:
+#                 print(i, end = " ")
+#             print()
+#     except KeyError:
+#         print("\nERROR\nThe given Instructions/Registors are not VALID!")
+#         exit()
+#     except:
+#         print("\nERROR\nGeneral Syntax Error")
+
+
+label = {}
+variables = {}
+code=[]
+initialcode=[]
+temp=[]
+count=0
+
+
+def storeAddress():
+    global count
+    for i in range(0,len(initialcode)):
+        command=initialcode[i].strip().split()
+        if len(command)==0:
+            continue
+        if command[0]=='var':
+            if(len(command)==1):
                 continue
-
-            if temp[0] == "mov":
-                if (temp[2][0]) == "$":
-                    printTypeB(temp[0], temp[1], temp[2])
-                    track += 1
-                    continue
-                else:
-                    printTypeC(temp[0], temp[1], temp[2])
-                    track += 1
-                    continue
-
-            if codes[temp[0]][1] == 'A':
-                printTypeA(temp[0], temp[1], temp[2], temp[3])
-                track += 1
-
-            if codes[temp[0]][1] == 'B':
-                printTypeB(temp[0], temp[1], temp[2])
-                track += 1
-
-            if codes[temp[0]][1] == 'C':
-                printTypeC(temp[0], temp[1], temp[2])
-                track += 1
-
-            if (codes[temp[0]][1] == 'D'):
-                if (temp[2] in variables):
-                    printTypeD(temp[0], temp[1], temp[2])
-                    track += 1
-                else:
-                    print("\nERROR\nUndefined Varaible Used!")
-                    exit()
-
-            if temp[0] == "jmp" or temp[0] == "jlt" or temp[0] == "jgt" or temp[0] == "je":
-                printTypeB(temp[0], temp[1])
-                track += 1
-                continue
-
-            if codes[temp[0]][1] == 'F':
-                printTypeF(temp[0])
-                track += 1
-                break
-
-        if track != len(data):
-            print("\nERROR\nLast Instruction is required to be HLT")
-            exit()
-        if temp != ['hlt']:
-            print("\nERROR\nHLT Instruction Missing!")
+            else:
+                variables[command[1]]=None
         else:
-            print(f"\nNumber of Variables: {variablecount}")
-            print("Variables:-", end = " ")
-            for i in variables:
-                print(i, end = " ")
-            print()
-    except KeyError:
-        print("\nERROR\nThe given Instructions/Registors are not VALID!")
-        exit()
-    except:
-        print("\nERROR\nGeneral Syntax Error")
+            if ':' in command[0]:
+                code.append(command[1:])
+                temp.append(command)
+                count+=1
+            else:
+                code.append(command)
+                temp.append(command)
+                count+=1
+
+#Generates Address for the variable and stores in a dictionary
+    for i in variables:
+        variables[i]=decimalTo8bitBinary(count)
+        count+=1
+
+#Generates Address for the label and stores in a dictionary
+    for i in range(0,len(temp)):
+        if ':' in temp[i][0]:
+            labelname=temp[i][0]
+            label[labelname.rstrip(':')]=decimalTo8bitBinary(i)
+
+#Main program which takes the input from the user and handles empty lines
+if __name__== "__main__":
+    while True:
+        try:
+            line=input()
+            if(len(line)!=0):
+                initialcode.append(line)
+        except EOFError:
+            break
